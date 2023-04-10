f029b8d27d2a7284b33b1bebf122d1ebb7e0afff
Leksa <lexysnake@gmail.com>
2023-04-11 01:40:31 +0300

    set db

--- api/db.js
+++ api/db.js
@@ -1,12 +1,8 @@
 const db = require("mongoose");
-
-const userName = process.env.DBUSER;
-const userPass = process.env.DBPWD;
 const dbHost = process.env.DBHOST;
 const dbPort = process.env.DBPORT;
 const dbName = process.env.DBNAME;
-
-const url = `mongodb://${userName}:${userPass}@${dbHost}:${dbPort}/${dbName}?authSource=admin&retryWrites=true&w=majority`;
+const url = `mongodb://${dbHost}:${dbPort}/${dbName}?authSource=admin&retryWrites=true&w=majority`;
 
 db.connect(url, {
     useNewUrlParser: true,
