2fc238841b51433b7b58cdd7b93ef0c7557e6b7b
Leksa <lexysnake@gmail.com>
2023-04-11 03:13:19 +0300

    set refresh routes

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 require("dotenv").config()
 const express = require("express");
 const cors = require("cors");
+var path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.json({limit: "50mb"}));
 app.use(express.urlencoded({extended: true, limit: "50mb"}));
 app.use(express.static("./dist"));
-app.use(cors())
+app.use(cors());
 app.use("/api", require("./api/routes"));
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'dist/index.html'), function(err) {
+        if (err) {
+            res.status(500).send(err)
+        }
+    })
+})
 app.listen(PORT);
