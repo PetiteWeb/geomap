91f1b6b68995353cc586252dc582d90bd7a0947f
Leksa <lexysnake@gmail.com>
2023-04-11 13:52:22 +0300

    upd

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,7 @@ module.exports = {
     devServer: {
         contentBase: path.join(__dirname, "dist"),
         historyApiFallback: true,
+        compress: true,
         port: 8081,
         open: true
     },
